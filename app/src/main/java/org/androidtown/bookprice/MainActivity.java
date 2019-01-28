abc09235e4d06176c017a0c930512f17a70e1ae5
seosungjoon <seojj95@naver.com>
2019-01-28 21:53:53 +0900

    [추가]
    -카메라실행버튼추가
    -이미지뷰추가
    -카메라실행후 촬영시 썸네일불러오기
    !!파일접근권한부분 다시조정할것

--- app/src/main/java/org/androidtown/bookprice/MainActivity.java
+++ app/src/main/java/org/androidtown/bookprice/MainActivity.java
@@ -1,13 +1,111 @@
 package org.androidtown.bookprice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_IMAGE_CAPTURE = 1001;
+    final int CAMERA_REQUEST_CODE = 1;
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+
     }
+
+    public void button_openCam(View v){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                            PackageManager.PERMISSION_GRANTED ||
+                            checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                                    PackageManager.PERMISSION_GRANTED)
+            {
+                // Should we show an explanation?
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE))
+                {
+                    // ...
+                }
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
+            }
+            else
+            {
+                ;
+            }
+            int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.CAMERA);
+            if(permissionCheck== PackageManager.PERMISSION_DENIED){
+                //권한없음
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},0);
+
+            }else{
+                //권한 있음
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+        else
+        {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+
+        }
+    }
+
+    public void button_checkPic(View v){
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if(requestCode == 0){
+            if(grantResults[0] == 0){
+                Toast.makeText(this,"카메라 권한이 승인됨",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this,"카메라 권한이 거절 되었습니다.",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap)bundle.get("data");
+            imageView.setImageBitmap(bitmap);
+
+
+
+/*            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 8;
+            if (file != null) {
+                Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+
+                imageView.setImageBitmap(bitmap);
+            } else {
+                Toast.makeText(getApplicationContext(), "File is null.", Toast.LENGTH_LONG).show();
+            }*/
+        }
+    }
+
+
+
 }
