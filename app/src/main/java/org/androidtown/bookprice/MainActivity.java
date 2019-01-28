45d59eb6297a5de95488ef4fee4a763231f8dc75
seosungjoon <seojj95@naver.com>
2019-01-28 23:16:31 +0900

    [추가,수정]
    -사진촬영후 사진 원본이 보이는 xml파일추가
    -사진촬영후 사진 원본이 보이게 코드추가 및 수정
    -!!권한관련 어플 시작할때 묻도록 변경할것

--- app/src/main/java/org/androidtown/bookprice/MainActivity.java
+++ app/src/main/java/org/androidtown/bookprice/MainActivity.java
@@ -3,23 +3,31 @@ package org.androidtown.bookprice;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_IMAGE_CAPTURE = 1001;
-    final int CAMERA_REQUEST_CODE = 1;
 
     ImageView imageView;
+    private String imageFilePath;
+    private Uri photoUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,46 +35,76 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = (ImageView) findViewById(R.id.imageView);
 
+        Button btn = this.findViewById(R.id.button);
+        Button btn1= this.findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button_openCam(view);
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button_checkPic(view);
+            }
+        });
+
 
     }
 
     public void button_openCam(View v){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-        {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)//권한체크부분 오류
+        {                                                 //버튼 두번눌러야 실행됨->아예 어플실행할시 권한묻게 변경할것
+            int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.CAMERA);
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
                             PackageManager.PERMISSION_GRANTED ||
                             checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) !=
-                                    PackageManager.PERMISSION_GRANTED)
+                                    PackageManager.PERMISSION_GRANTED || permissionCheck== PackageManager.PERMISSION_DENIED)
             {
-                // Should we show an explanation?
-                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE))
-                {
-                    // ...
-                }
-                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                                Manifest.permission.READ_EXTERNAL_STORAGE},
-                        1);
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.CAMERA},
+                        0);
             }
-            else
-            {
+            else{
                 ;
             }
-            int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.CAMERA);
-            if(permissionCheck== PackageManager.PERMISSION_DENIED){
-                //권한없음
-                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},0);
-
-            }else{
-                //권한 있음
+            //권한 있음
+            if(ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        //Error occurred while creating the File
+                    }
+
+                    if (photoFile != null) {
+                        photoUri = FileProvider.getUriForFile(this, getPackageName(), photoFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
             }
         }
         else
         {
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getPackageManager())!=null) {
+                File photoFile = null;
+                try{
+                    photoFile = createImageFile();
+                }catch (IOException ex){
+                    //Error occurred while creating the File
+                }
 
+                if(photoFile !=null){
+                    photoUri = FileProvider.getUriForFile(this,getPackageName(),photoFile);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
         }
     }
 
@@ -74,38 +112,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-        if(requestCode == 0){
-            if(grantResults[0] == 0){
-                Toast.makeText(this,"카메라 권한이 승인됨",Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(this,"카메라 권한이 거절 되었습니다.",Toast.LENGTH_SHORT).show();
-            }
-        }
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "TEST_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, /*prefix */
+                ".jpg", /* suffix */
+                storageDir      /*directory*/
+        );
+        imageFilePath = image.getAbsolutePath();
+        return image;
     }
+
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle bundle = data.getExtras();
-            Bitmap bitmap = (Bitmap)bundle.get("data");
-            imageView.setImageBitmap(bitmap);
+            ((ImageView)findViewById(R.id.imageView)).setImageURI(photoUri);
 
-
-
-/*            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 8;
-            if (file != null) {
-                Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-
-                imageView.setImageBitmap(bitmap);
-            } else {
-                Toast.makeText(getApplicationContext(), "File is null.", Toast.LENGTH_LONG).show();
-            }*/
         }
     }
 
-
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+//        if(requestCode == 0){
+//            if(grantResults[0] == 0){
+//                Toast.makeText(this,"카메라 권한이 승인됨",Toast.LENGTH_SHORT).show();
+//            }else{
+//                Toast.makeText(this,"카메라 권한이 거절 되었습니다.",Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 
 }
